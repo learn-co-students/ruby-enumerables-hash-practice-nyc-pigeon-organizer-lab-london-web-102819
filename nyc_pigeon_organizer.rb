a42d872c46f34f6be9621cd1084da47f9e4103ec
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-17 13:29:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |color_gender_lives, stats|
+    stats.each do |key, names|
+      names.each do |name| 
+        if !new_hash[name] 
+          new_hash[name] = {}
+        end 
+        if !new_hash[name][color_gender_lives] 
+          new_hash[name][color_gender_lives] = []
+        end
+        new_hash[name][color_gender_lives].push(key.to_s)
+      end
+    end
+  end
+  new_hash
 end
+

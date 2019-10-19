cfb329ff16aab669948d7bea85d272d967816e00
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 09:20:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ pigeon_list={}
+ data.each{|prop_name, prop_values|
+  prop_values.each{|spec_value, pigeons|
+    pigeons.each{|name|
+      if pigeon_list.key?(name)==false
+        pigeon_list.store(name,{})      
+      end
+     if pigeon_list[name].key?(prop_name)==false
+       pigeon_list[name].store(prop_name,[])
+     end
+     pigeon_list[name][prop_name].push(spec_value.to_s)
+    }
+  }
+ }
+ 
+ return pigeon_list
 end
